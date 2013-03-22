4ae6cc359cf5be4b055954b80aba96cd04ccf8d8
Charles Moulliard <cmoulliard@fusesource.com>
2013-03-22 10:34:52 +0100

    Big refactoring, add exceptions, cbr, unit test

---
+++ camel/camel-exercise-standalone/src/main/java/org/jboss/fuse/training/camel/MainApp1.java
@@ -0,0 +1,72 @@
+package org.jboss.fuse.training.camel;
+
+import org.apache.camel.*;
+import org.apache.camel.builder.DefaultErrorHandlerBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.*;
+import org.apache.camel.spi.RouteContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class MainApp1 {
+
+    private static Logger logger = LoggerFactory.getLogger(MainApp1.class);
+
+    public static void main(String[] args) throws Exception {
+
+        // CamelContext = container where we will register the routes
+        DefaultCamelContext camelContext = new DefaultCamelContext();
+
+        RouteBuilder routeBuilder = new RouteBuilder() {
+
+            // A Route sending every x seconds a message
+            // to the logger processor
+
+            @Override
+            public void configure() throws Exception {
+                from("timer://exercise1delay=1000")
+                        .log(LoggingLevel.INFO, ">> This is the first Camel exercise")
+                        .process(new Processor() {
+                            public void process(Exchange exchange) throws Exception {
+                                logger.info("Invoked timer at " + new Date());
+                            }
+                        });
+            }
+        };
+
+        // We will use RouteDefinition
+        // Not recommended way
+        RouteDefinition rd = new RouteDefinition();
+        rd.setExchangePattern(ExchangePattern.InOnly);
+        rd.setAutoStartup("true");
+        rd.setErrorHandlerBuilder(new DefaultErrorHandlerBuilder());
+        rd.setTrace("false");
+
+        FromDefinition from = new FromDefinition("timer://exercise2");
+        List<FromDefinition> fromDefinitionList = new ArrayList<FromDefinition>();
+        fromDefinitionList.add(from);
+
+        LogDefinition log = new LogDefinition().log(LoggingLevel.INFO, ">>> Using route definition");
+        List<ProcessorDefinition<?>> processorDefinitions = new ArrayList<ProcessorDefinition<?>>();
+        processorDefinitions.add(log);
+
+        rd.setInputs(fromDefinitionList);
+        rd.setOutputs(processorDefinitions);
+
+        camelContext.addRouteDefinition(rd);
+        camelContext.addRoutes(routeBuilder);
+
+        // Start the container
+        camelContext.start();
+
+        // give it time to realize it has work to do
+        Thread.sleep(20000);
+
+    }
+}
